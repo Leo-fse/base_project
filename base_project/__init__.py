472e10a34a472ac3a592ec4dafc1c178b501bc37
tmk <morimoto.ke@gmail.com>
2022-01-10 12:30:28 +0900

    🎨 logging設定ファイルをPyファイルに変更

--- base_project/__init__.py
+++ base_project/__init__.py
@@ -7,7 +7,10 @@ import yaml
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "."))
 
+from config.logging_comfig import loggging_config  # NOQA
+
 from settings import LOG_CONFIG_PATH  # NOQA
 
 # logging.config.fileConfig(LOG_CONFIG_PATH, disable_existing_loggers=False)
-logging.config.dictConfig(yaml.safe_load(open(LOG_CONFIG_PATH).read()))
+# logging.config.dictConfig(yaml.safe_load(open(LOG_CONFIG_PATH).read()))
+logging.config.dictConfig(loggging_config)
---
+++ config/logging_comfig.py
@@ -0,0 +1,71 @@
+from base_project.settings import LOG_DIR
+
+"""loggingの設定
+    iniファイル：
+        Filterの設定できない
+        書き方がちょっと独特
+    yamlファイル：
+        辞書型に読み取れるなら何でもいいらしいのでそれだったら
+        pyファイルでいいんじゃない
+    py(辞書型)：
+        jsonとかでもいいけど、pyファイルにしといて方が出力さきとかを
+        ほかのpyファイルから動的に読み取れるからこっちのほうが良さそう。
+
+    ****注意事項****
+    [propagate]
+    loggersで propagate を False にしておかないと親ロガーも継承する。
+    例えば root で設定しているhandlerを個別ロガーで除外しても、
+    個別側でも継承され適用され続ける。
+
+    [disable_existing_loggers]
+    Falseにしておかないと困ったという人が多いみたい。
+    loggerConfigの読み込むタイミングによってうまく動かないときがあるもよう。
+    僕のに関しては__init__でログ設定してしまうので問題ないが、明示的にFalseに
+    設定しておく
+"""
+
+loggging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "DEBUG", "handlers": ["console", "file"]},
+    "loggers": {
+        "errorLog": {"level": "ERROR", "handlers": ["errorFile"], "propagate": False},
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "level": "DEBUG",
+            "formatter": "simple",
+            "filename": f"{LOG_DIR}/log.log",
+            "mode": "w",
+            "encoding": "utf-8",
+        },
+        "errorFile": {
+            "class": "logging.FileHandler",
+            "level": "ERROR",
+            "formatter": "error",
+            "filename": f"{LOG_DIR}/error.log",
+            "mode": "w",
+            "encoding": "utf-8",
+        },
+    },
+    "formatters": {
+        "error": {
+            "format": "%(asctime)s - %(levelname)s - %(message)s",
+            "datefmt": "%Y-%m-%d %I:%M:%S %p",
+        },
+        "simple": {
+            "format": (
+                "%(asctime)s - %(levelname)-8s - %(name)-12s - %(module)-10s - "
+                "%(funcName)-10s - %(lineno)-3s - %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %I:%M:%S %p",
+        },
+    },
+}
