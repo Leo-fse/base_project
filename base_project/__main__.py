ec5e598795c14fd2ae14efd995ce0269207fe562
tmk <morimoto.ke@gmail.com>
2022-01-10 14:12:56 +0900

    ✨ じゃんけん実行用コード追加

--- base_project/__main__.py
+++ base_project/__main__.py
@@ -1,21 +1,10 @@
 import logging
 
-from libs.test import main
-from libs.utils.c_logger import convert_traceback_to_oneline
+from libs.janken import janken
 
 logger = logging.getLogger(__name__)
-err_logger = logging.getLogger("errorLog")
-
-
-def devide(a, b):
-    try:
-        print(f"result: { a / b } expression: resutl = {a} / {b}")
-    except Exception as e:
-        logger.error(e)
-        err_logger.error(f"{e} a = {a}, b = {b}", exc_info=True)
+e_logger = logging.getLogger("errorLog")
 
 
 if __name__ == "__main__":
-    devide(1, 0)
-    main(3, 5)
-    main("uuu", 1)
+    janken.main()
